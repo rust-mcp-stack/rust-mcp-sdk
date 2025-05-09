c0b6f205c88578ad0feafcf03ad040e21b32abec
Ali Hashemi <programmer.ice@gmail.com>
2025-05-09 18:06:45 -0300

    tests: add new tests for client runtime

---
+++ crates/rust-mcp-sdk/tests/common/common.rs
@@ -0,0 +1,24 @@
+use async_trait::async_trait;
+use rust_mcp_schema::{
+    ClientCapabilities, Implementation, InitializeRequestParams, JSONRPC_VERSION,
+};
+use rust_mcp_sdk::mcp_client::ClientHandler;
+
+pub const NPX_SERVER_EVERYTHING: &str = "@modelcontextprotocol/server-everything";
+pub const UVX_SERVER_GIT: &str = "mcp-server-git";
+
+pub fn test_client_info() -> InitializeRequestParams {
+    InitializeRequestParams {
+        capabilities: ClientCapabilities::default(),
+        client_info: Implementation {
+            name: "test-rust-mcp-client".into(),
+            version: "0.1.0".into(),
+        },
+        protocol_version: JSONRPC_VERSION.into(),
+    }
+}
+
+pub struct TestClientHandler;
+
+#[async_trait]
+impl ClientHandler for TestClientHandler {}
