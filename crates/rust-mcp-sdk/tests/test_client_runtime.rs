af48a2baca78a7d3cf16cc79c5591f8534332881
Ali Hashemi <14126952+hashemix@users.noreply.github.com>
2025-05-09 19:22:02 -0300

    test: add new tests for client runtime (#29)
    * tests: add new tests for client runtime
    
    * test: update workflow to test on both linux and windows
    
    * chore: disable node cache
    
    * chore: disable python cache
    
    * chore: update ci workflow
    
    * chore: disable unnecessary test on windows
    
    * chore: update ci workflow
    
    * chore: run clippy on both windows and linux
    
    * chore: fix build issue

---
+++ crates/rust-mcp-sdk/tests/test_client_runtime.rs
@@ -0,0 +1,56 @@
+use common::{test_client_info, TestClientHandler, NPX_SERVER_EVERYTHING};
+use rust_mcp_sdk::{mcp_client::client_runtime, McpClient, StdioTransport, TransportOptions};
+
+#[cfg(unix)]
+use common::UVX_SERVER_GIT;
+
+#[path = "common/common.rs"]
+pub mod common;
+
+#[tokio::test]
+async fn tets_client_launch_npx_server() {
+    // NPM based MCP servers should launch successfully using `npx`
+    let transport = StdioTransport::create_with_server_launch(
+        "npx",
+        vec!["-y".into(), NPX_SERVER_EVERYTHING.into()],
+        None,
+        TransportOptions::default(),
+    )
+    .unwrap();
+
+    let client = client_runtime::create_client(test_client_info(), transport, TestClientHandler {});
+
+    client.clone().start().await.unwrap();
+
+    let server_capabilities = client.server_capabilities().unwrap();
+    let server_info = client.server_info().unwrap();
+
+    assert!(server_info.server_info.name.len() > 0);
+    assert!(server_info.server_info.version.len() > 0);
+    assert!(server_capabilities.tools.is_some());
+}
+
+#[cfg(unix)]
+#[tokio::test]
+async fn tets_client_launch_uvx_server() {
+    // The Python-based MCP server should launch successfully
+    // provided that `uvx` is installed and accessible in the system's PATH
+    let transport = StdioTransport::create_with_server_launch(
+        "uvx",
+        vec![UVX_SERVER_GIT.into()],
+        None,
+        TransportOptions::default(),
+    )
+    .unwrap();
+
+    let client = client_runtime::create_client(test_client_info(), transport, TestClientHandler {});
+
+    client.clone().start().await.unwrap();
+
+    let server_capabilities = client.server_capabilities().unwrap();
+    let server_info = client.server_info().unwrap();
+
+    assert!(server_info.server_info.name.len() > 0);
+    assert!(server_info.server_info.version.len() > 0);
+    assert!(server_capabilities.tools.is_some());
+}
